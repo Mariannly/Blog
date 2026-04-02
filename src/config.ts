a0fff84bef8e405473bda616fff21282239e43fb
Mariannly <mariannly.merquez@gmail.com>
2026-04-01 21:56:39 -0500

    Added CV

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ import { LinkPreset } from "./types/config";
 export const siteConfig: SiteConfig = {
 	title: "Mariannly's Blog",
 	subtitle:
-		"Aspiring Theoretical Physicist | Occasional Programmer | Apprentice writer |Lifelong learner",
+		"Aspiring Theoretical Physicist | Occasional Programmer | Apprentice writer | Lifelong learner",
 	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 135, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -56,7 +56,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/profile_pic.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Mariannly Marquez-Pirela",
-	bio: "Aspiring Theoretical Physicist | Occasional Programmer | Apprentice writer |Lifelong learner",
+	bio: "Aspiring Theoretical Physicist | Occasional Programmer | Apprentice writer | Lifelong learner",
 	links: [
 		{
 			name: "CV",
